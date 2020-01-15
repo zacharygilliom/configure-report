7fe7195b0e1ed7c86c0599e178eceb0426f80ce9
zacharygilliom <zacharygilliom@gmail.com>
2020-01-14 22:25:54 -0500

    cleaned up functions

--- main.py
+++ main.py
@@ -13,30 +13,17 @@ def add_columns(df):
 	df['Started to Submitted'] = ''
 	return df	
 
+def merge_workbooks(df_new, df):
+	df_final = drop_columns(df)
+	df_new = drop_columns(df_new)
+	df_new = add_columns(df_new)
+	df_final = df_final.append(df_new, ignore_index=True)
+	return df_final
+
+
 df = pd.read_excel('Master Submitted Log.xlsx')
-df = drop_columns(df)
-df = add_columns(df)
-for row in df:
-	df['Received to Submitted'] = 1
-print(df.head())
+df_new = pd.read_excel('Master Submitted Log New.xlsx')
 
-# # def calculate_column(df):
-# 	for row in df:
-
-
-# def merge_workbooks():
-# 	df_final = pd.read_excel('C:\\Users\\zacha\\PycharmProjects\\configure-report\\Master Submitted Log Final.xlsx')
-# 	df_final = drop_columns(df_final)
-# 	df_new = pd.read_excel('C:\\Users\\zacha\\PycharmProjects\\configure-report\\Master Submitted Log.xlsx')
-# 	df_new = drop_columns(df_new)
-# 	df_new = add_columns(df_new)
-# 	df_final = df_final.append(df_new, ignore_index=True)
-# 	df_final.to_excel('C:\\Users\\zacha\\PycharmProjects\\configure-report\\Master Submitted Log Final.xlsx')
-# 	df_final.to_csv(path_or_buf='C:\\Users\\zacha\\PycharmProjects\\configure-report\\MasterSubmittedCSV.csv', index=False)
-# 	return df_final
-# 	# return file
-
-# print(merge_workbooks())
-
-
+final_df = merge_workbooks(df_new, df)
+print(final_df.head(20))
 
